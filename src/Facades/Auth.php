0b3be11e66af57f2ab910c64bbece442aafb7e57
Andrey Helldar <helldar@ai-rus.com>
2021-07-21 22:06:14 +0300

    Removed unnecessary dependencies

--- src/Facades/Auth.php
+++ src/Facades/Auth.php
@@ -4,7 +4,7 @@ namespace Helldar\CashierDriver\Tinkoff\Auth\Facades;
 
 use Helldar\CashierDriver\Tinkoff\Auth\DTO\Client;
 use Helldar\CashierDriver\Tinkoff\Auth\Support\Auth as Support;
-use Illuminate\Support\Facades\Facade;
+use Helldar\Support\Facades\Facade;
 
 /**
  * @method static array accessToken(Client $client)
